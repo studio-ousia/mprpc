edcb693634c2a09979d3578dd5059dbc5563de78
Ikuya Yamada <ikuya@ikuya.net>
2014-09-17 22:50:26 +0900

    fix example code

--- examples/sum_client.py
+++ examples/sum_client.py
@@ -3,19 +3,18 @@
 import gsocketpool.pool
 import gevent.pool
 
-from mprpc import RPCClient
+from mprpc import RPCClient, RPCPoolClient
 
 
 def call():
     client = RPCClient('127.0.0.1', 6000)
-    client.open()
 
     print client.call('sum', 1, 2)
 
 
 def call_using_pool():
     options = dict(host='127.0.0.1', port=6000)
-    client_pool = gsocketpool.pool.Pool(RPCClient, options)
+    client_pool = gsocketpool.pool.Pool(RPCPoolClient, options)
 
     def _call(n):
         with client_pool.connection() as client:

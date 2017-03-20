56d521ff768e303658485ecf997b6ffd51779976
Ikuya Yamada <ikuya@ikuya.net>
2017-03-21 01:36:22 +0900

    bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, Extension, find_packages
 
 setup(
     name='mprpc',
-    version='0.1.10',
+    version='0.1.11',
     description='A fast MessagePack RPC library',
     long_description=open('README.rst').read(),
     author='Studio Ousia',

5aef111915891bf821ad340ac0a949f4fbc5bc36
Ikuya Yamada <ikuya@ikuya.net>
2017-10-16 10:28:16 +0900

    bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, Extension
 
 setup(
     name='mprpc',
-    version='0.1.14',
+    version='0.1.15',
     description='A fast MessagePack RPC library',
     long_description=open('README.rst').read(),
     author='Studio Ousia',
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     keywords=['rpc', 'msgpack', 'messagepack', 'msgpackrpc', 'messagepackrpc',
               'messagepack rpc', 'gevent'],
-    classifiers=(
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
@@ -30,7 +30,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-    ),
+    ],
     install_requires=[
         'gsocketpool',
         'gevent',

studio-ousia/mprpc

commit 227231f358e5c63d527739ee5234459305a5f619

Fix defaults for msgpack-python with respect to blobs
msgpack-python defaults to using binary strings, unless an encoding is
specified. In that case, `use_bin_type=True` needs to also be specified
to allow blobs to be serialized as well as strings.

mprpc specifies an encoding, but not `use_bin_type`, which this commit
rectifies. The default setting now allows Unicode strings well as
arbitrary blobs.

mprpc/client.pyx
@@ -61,7 +61,7 @@ cdef class RPCClient:
         self._socket = None
         self._tcp_no_delay = tcp_no_delay
         self._keep_alive = keep_alive
-        self._pack_params = pack_params or dict()
+        self._pack_params = pack_params or dict(use_bin_type=True)
         self._unpack_encoding = unpack_encoding
         self._unpack_params = unpack_params or dict(use_list=False)
 

mprpc/server.pyx
@@ -44,7 +44,7 @@ cdef class RPCServer:
 
     def __init__(self, *args, **kwargs):
         pack_encoding = kwargs.pop('pack_encoding', 'utf-8')
-        pack_params = kwargs.pop('pack_params', dict())
+        pack_params = kwargs.pop('pack_params', dict(use_bin_type=True))
 
         self._unpack_encoding = kwargs.pop('unpack_encoding', 'utf-8')
         self._unpack_params = kwargs.pop('unpack_params', dict(use_list=False))

commit 92c410fac9a186f022f9d5e9ac984a11b5af8ae3

Fix Py2k method name type
The fix introduced in 227231f3 introduced another bug: By enabling
binary strings, Python 2 would serialize the method name as a binary
string, not UTF-8 as according to the standard.
An obvious way to make this fail is by running the server with Python 3
and the client with Python 2.

This conditionally converts strings to UTF-8 when using Python 2.

Signed-off-by: Martin Braun <martin.braun@ettus.com>

mprpc/client.pyx
@@ -3,6 +3,7 @@
 
 import msgpack
 import time
+import sys
 from gevent import socket
 from gsocketpool.connection import Connection
 
@@ -120,12 +121,16 @@ cdef class RPCClient:
         else:
             return False
 
-    def call(self, str method, *args):
+    def call(self, str method_str, *args):
         """Calls a RPC method.
 
         :param str method: Method name.
         :param args: Method arguments.
         """
+        if sys.version_info.major >= 3:
+            method = method_str
+        else:
+            method = unicode(method_str)
 
         cdef bytes req = self._create_request(method, args)
 
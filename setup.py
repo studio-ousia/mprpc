ffe9ebc475bc4bf92e517e5ba3f9711204f1ffc2
Yukino Ikegami <yknikgm@gmail.com>
2020-04-11 00:33:38 +0900

    support Python 3.6, 3.7 and 3.8

--- setup.py
+++ setup.py
@@ -28,6 +28,9 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     install_requires=[
         'gsocketpool',
